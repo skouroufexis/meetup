691740d4e9b93edb2fb8b180832d9cb9dffd813d
skouroufexis <skouroufexis@gmail.com>
2020-08-12 20:12:43 +0300

    Updates

---
+++ precache-manifest.473d5a40f45ee746d63bd4539adfa4f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7989bb13bb7a7c4b728b957c632fc92",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "07bf98f94fc02b39f5f1",
+    "url": "/meetup/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa44b4cd9222c00b0b53",
+    "url": "/meetup/static/js/2.e3215a69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meetup/static/js/2.e3215a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07bf98f94fc02b39f5f1",
+    "url": "/meetup/static/js/main.7fbf3603.chunk.js"
+  },
+  {
+    "revision": "5b15bedc16c52474edcc",
+    "url": "/meetup/static/js/runtime-main.61f01ad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup/static/media/logo.5d5d9eef.svg"
+  }
+]);
