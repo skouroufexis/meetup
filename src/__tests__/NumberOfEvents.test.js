56d69076f5b523d6c8f5ce878ec530c7f11118e5
skouroufexis <skouroufexis@gmail.com>
2020-08-23 22:07:04 +0300

    Create NumberOfEvents.test.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+let NumberOfEventsWrapper;
+describe('<NumberOfEvents /> component', () => {
+
+    beforeAll(() => {
+         NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+         
+      });  
+
+
+  test('render number of events text input', () => {
+    
+    expect(NumberOfEventsWrapper.find('#input_numberOfEvents')).toHaveLength(1);
+  });
+
+  test('render number of events text input label', () => {
+    
+    expect(NumberOfEventsWrapper.find('#numberOfEventsLabel')).toHaveLength(1);
+  });
+
+  test('Initial value of number of events = 32', () => {
+    
+    expect(NumberOfEventsWrapper.state('number')).toBe(32);
+  });
+
+
+  test('change state when text input changes', () => {
+    
+    const eventObject = { target: { value: 10 }};
+    NumberOfEventsWrapper.find('#input_numberOfEvents').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('number')).toBe(10);
+  });
+
+
+
+
+
+
+
+  
+  
+});
+
