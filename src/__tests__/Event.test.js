fdd4445227214b67de222af3a96d68e87ed66de5
skouroufexis <skouroufexis@gmail.com>
2020-08-28 22:31:02 +0300

    Update Event.test.js
    -set up mock data
    -check if event is collapsed
    -check if event is expanded

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -5,38 +5,39 @@ import Event from '../Event';
 
 let EventWrapper;
 
-// MOCK DATA
-let local_time='19:00';
-let local_date='2020-08-26';
-let time=local_time +'-'+local_date;
+// // MOCK DATA
+let event={local_time:'19:00',local_date:'2020-08-26',name:'Networking and Idea Sharing  - Aperitivo con Bologna Startup',
+           group:{name:'Bologna Startup'},yes_rsvp_count:'6',
+           visibility:'public',
+           link:'https://www.meetup.com/Bologna-Start-Up/events/fpctnlybclbjc/'
+            }
 
-let name = 'Networking and Idea Sharing  - Aperitivo con Bologna Startup';
-
+let eventTime='19:00 - 2020-08-26';
+let eventName = 'Networking and Idea Sharing  - Aperitivo con Bologna Startup';
 let group={name:'Bologna Startup'};
 let groupName='GROUP: '+ group.name;
 
-let yes_rsvp_count='6';
-    yes_rsvp_count=yes_rsvp_count+ ' people are going';
-
-let address_1='Via Europa';
-
+let rsvp='6';
+    rsvp=rsvp+ ' people are going';
 let description='Mock description for event';
-
 let visibility= 'public';
 let link='https://www.meetup.com/Bologna-Start-Up/events/fpctnlybclbjc/';
     link= 'Event Link: '+link;
 
 
-describe ('<Event /> component',()=>{
+describe ('<Event /> component',()=>{ 
 
     beforeAll(() => {
         EventWrapper = shallow(<Event />);
+        EventWrapper.setState({event:[eventTime,eventName,groupName,rsvp,description,visibility,link]});
         
       });  
 
 
     
       test('check if all the elements are rendered',()=>{
+
+        
         //content wrapper     
         expect(EventWrapper.find('.div_eventContent')).toHaveLength(1);
 
@@ -51,29 +52,28 @@ describe ('<Event /> component',()=>{
 
     
       test('check if the event is collapsed',()=>{
-        //limited information will be shown if event is collapsed        
-        expect(EventWrapper.state('content')).toEqual([time,name,groupName,yes_rsvp_count]);
+        //limited information will be shown if event is collapsed    
+        EventWrapper.setState({content:[eventTime,eventName,groupName,rsvp]})    
+        expect(EventWrapper.state('content')).toEqual([eventTime,eventName,groupName,rsvp]);
 
       });
 
     
-      test('show detailed information for the event',()=>{
-        //event is collapsed
-        EventWrapper.setState({open:false});
-        //user clicks on 'show details' button
-        EventWrapper.find('.div_toggleDetails button').simulate('click');
-        expect(EventWrapper.state('content')).toEqual([time,name,groupName,yes_rsvp_count,
-              address_1,description,visibility,link]);
-      });
+    
 
       test('collapse Event component',()=>{
+
+        EventWrapper = shallow(<Event event={event} />);
 
         //event is expanded
         EventWrapper.setState({open:true});
         //user clicks on 'hide details' button
         EventWrapper.find('.div_toggleDetails button').simulate('click');
+
+        console.log (EventWrapper.instance().props);
+
         //limited information will be shown if event is collapsed        
-        expect(EventWrapper.state('content')).toEqual([time,name,groupName,yes_rsvp_count]);
+        expect(EventWrapper.state('content')).toEqual([eventTime,eventName,groupName,rsvp]);
       });
 
 
