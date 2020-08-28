86c190a2e1087aa84299b2b2052a2725cd8b809b
skouroufexis <skouroufexis@gmail.com>
2020-08-28 22:33:40 +0300

    Update CitySearch.js
    -render correct suggestions
    -remove suggestion list once city is selected

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getSuggestions } from './api';
 
 class CitySearch extends Component {
     state = {
@@ -10,10 +11,12 @@ class CitySearch extends Component {
       handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({ query: value });
+        getSuggestions(value).then(suggestions => this.setState({ suggestions }));
       }
 
-      handleItemClicked = (value) => {
-        this.setState({ query: value });
+      handleItemClicked = (value,lat, lon) => {
+        this.setState({ query: value,suggestions:[]});
+        this.props.updateEvents(lat, lon);
       }
 
   render() {
@@ -21,12 +24,11 @@ class CitySearch extends Component {
       <div className="CitySearch">
           <input type="text" className="city" value={this.state.query} 
           onChange={this.handleInputChanged}
-
-
+          placeholder='city'
           />
           <ul className="suggestions">
             {this.state.suggestions.map(item =>
-                <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string)} >
+                <li key={item.name_string} onClick={() => this.handleItemClicked(item.name_string,item.lat, item.lon)} >
                     {item.name_string}
                 </li>
                 )}
