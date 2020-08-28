ecbffad8e9efa31aa3e0c0f616a9c216adf2ae90
skouroufexis <skouroufexis@gmail.com>
2020-08-28 22:32:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React , { Component } from 'react';
+import React , { Component} from 'react';
 
 import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
-import Event from './Event';
-import NumberOfEvents from './NumberOfEvents';
 
-function App() {
-  return (
-    <div className="App">
-       <CitySearch />
-       <NumberOfEvents />
-       <EventList />
-       <Event />
-    </div>
-  );
+import NumberOfEvents from './NumberOfEvents';
+import { render } from 'enzyme';
+import { getEvents } from './api';
+
+class App extends Component {
+
+  state = {events:[]}
+
+  render(){
+
+    return (
+      <div className="App">
+         <CitySearch  updateEvents={this.updateEvents} />
+         <NumberOfEvents />
+         <EventList events={this.state.events} />
+         {/* <Event /> */}
+      </div>
+    )
+  }
+  
+  updateEvents = (lat, lon) => {
+    getEvents(lat, lon).then(events => this.setState({ events:events }));
+  }
 }
 
 export default App;
