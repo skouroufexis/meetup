1cd91fc9e8441b08564c092d47690cff4b49894e
skouroufexis <skouroufexis@gmail.com>
2020-08-28 22:31:15 +0300

    Update EventList.test.js
    correct number of events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -6,9 +6,16 @@ import Event from '../Event';
 
 
 describe('<EventList /> component', () => {
-    test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList />);
-        EventListWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
-        expect(EventListWrapper.find(Event)).toHaveLength(4);
-      });
+
+
+  test('render correct number of events', () => {
+    const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}/>);
+    expect(EventListWrapper.find(Event)).toHaveLength(4);
+  });
+
+    // test('render correct number of events', () => {
+    //     const EventListWrapper = shallow(<EventList />);
+    //     EventListWrapper.setState({ events: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] });
+    //     expect(EventListWrapper.find(Event)).toHaveLength(4);
+    //   });
 });
