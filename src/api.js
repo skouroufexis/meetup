cf2bd5461b1ebb6251e934a60466a7491026b364
skouroufexis <skouroufexis@gmail.com>
2020-09-03 20:53:14 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -87,7 +87,7 @@ async function getSuggestions(query) {
     console.log(token);
     console.log(query);
     if (token) {
-      const url = 'https://api.meetup.com/find/locations?&sign=true&photo-host=public&query='+query+'&access_token='+ token;
+      const url = 'https://api.meetup.com/find/locations?sign=true&photo-host=public&query='+query+'&access_token='+ token;
       // const result = await axios.get(url);
       const result = fetch(url).then(function(data){
         console.log(data);
