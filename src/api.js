4af9d18a1e1ea5675e4d14711a3b34a9b62bd6f4
skouroufexis <skouroufexis@gmail.com>
2020-09-03 21:52:14 +0300

    Update api.js
    prepended  'http://cors.io/?' to the url to see if CORS problem is solved

--- src/api.js
+++ src/api.js
@@ -87,7 +87,7 @@ async function getSuggestions(query) {
     console.log(token);
     console.log(query);
     if (token) {
-      const url = 'https://api.meetup.com/find/locations?&sign=true&photo-host=public&query='
+      const url = 'http://cors.io/?https://api.meetup.com/find/locations?&sign=true&photo-host=public&query='
       + query
       + '&access_token=' + token;
     const result = await axios.get(url);
