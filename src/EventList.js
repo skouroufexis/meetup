e58ec353006abecf2174b42f2a5d928e8cecfc53
skouroufexis <skouroufexis@gmail.com>
2020-09-05 12:13:06 +0300

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -7,28 +7,36 @@ class EventList extends Component {
   render() {
     
     return (
-        <ul className="EventList">
-        {this.props.events.map(event =>
-          <li key={event.id}>
-            <Event className='Event' event={event} />
-          </li>
-        )}
-      </ul>
+      //   <ul className="EventList">
+      //   {this.props.events.map(event =>
+      //     <li key={event.id}>
+      //       <Event className='Event' event={event} />
+      //     </li>
+      //   )}
+      // </ul>
+
+      this.eventCounter()
       
       
     );
   }
 
 
-  // eventCounter(){
-  //   let totalEvents=this.props.events;
+  eventCounter(){
+    let totalEvents=this.props.events;
 
-  //   return <ul className="EventList">
-  //           <li key={1}>
-  //           <Event className='Event' event={totalEvents[2]} />
-  //         </li>
-  //         </ul>
-  // }
+    
+    totalEvents.map(te=>{
+
+      return (<ul className="EventList">
+            <li key={te.id}>
+            <Event className='Event' event={te} />
+          </li>
+          </ul>)
+
+    })
+    
+  }
 
 }
 
