522ee3439d15c54ea65c7ec8f7ed106abfc51f45
skouroufexis <skouroufexis@gmail.com>
2020-09-05 15:15:13 +0300

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -10,7 +10,7 @@ class EventList extends Component {
 
         let totalEvents=this.props.events;
         let n=this.props.eventsNumber;
-            if(n.trim()==''){n=32;}
+            if(n==''){n=32;}
             if(isNaN(n)){n=32;}
         let c;
         let shownEvents=[];
