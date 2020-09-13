35c6dbf4fad800850b1bc930e79bec412b61ecee
skouroufexis <skouroufexis@gmail.com>
2020-09-13 23:12:47 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as atatus from 'atatus-spa';
+atatus.config('bf59846400ca481389ac39a956631b46').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +16,6 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
+
