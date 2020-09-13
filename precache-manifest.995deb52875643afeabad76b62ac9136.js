7526ac58ec7fa5761e1f504f4a04e659df653870
skouroufexis <skouroufexis@gmail.com>
2020-09-13 23:49:16 +0300

    Updates

--- precache-manifest.995deb52875643afeabad76b62ac9136.js
+++ precache-manifest.995deb52875643afeabad76b62ac9136.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5b5a06a7f0f65bcc1ffef6943547d83",
+    "revision": "538d8faf6322929a73afb3286fb06eb7",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "6fc148728fa22a0cf42c",
+    "revision": "bde8377f846d13b620da",
     "url": "/meetup/static/css/main.f0c6d20b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/meetup/static/js/2.2f2f26b0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6fc148728fa22a0cf42c",
-    "url": "/meetup/static/js/main.1c00f7c1.chunk.js"
+    "revision": "bde8377f846d13b620da",
+    "url": "/meetup/static/js/main.d46c2441.chunk.js"
   },
   {
     "revision": "5b15bedc16c52474edcc",
