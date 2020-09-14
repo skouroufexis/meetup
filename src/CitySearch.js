d554bf909d9ed5a63aa3aaf1e9af635ba939b8cf
skouroufexis <skouroufexis@gmail.com>
2020-09-14 15:18:31 +0300

    Update CitySearch.js
    Add InfoAlert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getSuggestions } from './api' ;
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
@@ -11,7 +12,19 @@ class CitySearch extends Component {
         const value = event.target.value;
         this.setState({ query: value }); //changes the search text value
 
-        getSuggestions(value).then(suggestions => this.setState({ suggestions }));
+        getSuggestions(value).then(suggestions => {
+          this.setState({ suggestions });
+          if (value && suggestions.length === 0) 
+            {
+              this.setState({infoText: 'We can not find the city you are looking for. Please try another city'});
+            } 
+          else 
+            {
+              this.setState({
+                infoText: ''
+              });
+            }
+        });
         
       }
 
@@ -22,7 +35,14 @@ class CitySearch extends Component {
 
   render() {
     return (
+      
       <div className='container'>
+
+      <div className="CitySearch">
+        <InfoAlert text={this.state.infoText} />
+        
+      </div>
+
         <div className="CitySearch container">
             <input type="text" className="city" value={this.state.query} 
             onChange={this.handleInputChanged}
