e8b32f315f937ceb1407870803e2666da2cea662
skouroufexis <skouroufexis@gmail.com>
2020-09-14 15:18:42 +0300

    Update NumberOfEvents.js
    Add ErrorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import {ErrorAlert} from './Alert';
 
 class NumberOfEvents extends Component {
     state = {
-        number:32
+        number:32,
+        infoText:''
       }
 
 
@@ -11,6 +13,8 @@ class NumberOfEvents extends Component {
     return (
       <div className='container'>
         <div className='container'>
+
+          <ErrorAlert text={this.state.infoText} />
           <div className='row'>
             <label className='col-12'>No. of events displayed: </label>
             <input className='col-2'  id='input_numberOfEvents' type='text' value= {this.state.number} onChange={this.changeNumber}></input>
@@ -24,9 +28,21 @@ class NumberOfEvents extends Component {
   changeNumber=(event)=>{
     
     let n= event.target.value;
-    
     this.setState({number:n});
     this.props.updateEventsNumber(n);
+    if (n>0)
+      {        
+        this.setState({infoText:''})
+      }
+
+    else
+      {
+        this.setState({infoText:'Please enter a valid event number'})
+      }
+    
+
+
+    
   }
 
 }
