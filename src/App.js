ff0ea8c6184719d01b0cf4735beb98041f0422f1
skouroufexis <skouroufexis@gmail.com>
2020-09-17 20:14:44 +0300

    Update App.js
    Import and display the "Offline" alert when there is no internet connection

--- src/App.js
+++ src/App.js
@@ -8,26 +8,38 @@ import NumberOfEvents from './NumberOfEvents';
 import { render } from 'enzyme';
 import { getEvents } from './api';
 
+import {OfflineAlert} from './Alert';
+
 
 
 
 class App extends Component {
 
   state = {events:[],
-           eventsNumber:32
+           eventsNumber:32,
+           infoText:''
           }
 
   render(){
-
+    
+    
     return (
       <div className="App">
-        
+          <OfflineAlert text={this.state.infoText} />
          <CitySearch  updateEvents={this.updateEvents} />
          <NumberOfEvents updateEventsNumber={this.updateEventsNumber}  />
          <EventList events={this.state.events} eventsNumber={this.state.eventsNumber} />
          
       </div>
     )
+  }
+
+
+  componentDidMount(){
+    if(!navigator.onLine)
+          {
+            this.setState({infoText:'No internet connection! You are now viewing an cached version of the meetup application'});
+          }
   }
   
   updateEvents = (lat, lon) => {
@@ -38,9 +50,8 @@ class App extends Component {
 
   updateEventsNumber =(n)=>{
     this.setState({eventsNumber:n});
-    
-
   }
+
 }
 
 export default App;
