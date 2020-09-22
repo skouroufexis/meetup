10b1bf398bad5ee72eaa4efc422d8ef88e4126d6
skouroufexis <skouroufexis@gmail.com>
2020-09-22 22:35:42 +0300

    Update chart.js
    Created pie chart element for representing event attendees

--- src/chart.js
+++ src/chart.js
@@ -1,79 +1,63 @@
-import React , { Component} from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
-const data = [{name: 'Page A', value:100, value2:100},
-              {name: 'Page B', value:220,value2:90},
-              {name: 'Page C', value:225,value2:93}  ,
-              {name: 'Page D', value:312,value2:87}                 
-            
-              ];
+import React, { PureComponent } from 'react';
+import {ResponsiveContainer, PieChart, Pie, Legend, Tooltip,Cell,LabelList} from 'recharts';
 
-const renderLineChart = (
-  <LineChart width={500} height={400} data={data} margin={{top:20}}>
-    <Line type='monotone' dataKey="value2" stroke="red" />
-    
-    <CartesianGrid stroke='blue' />
-    <XAxis   stroke='darkorange' />
-    <YAxis  stroke='green' />
-  </LineChart>
-);
-
-
-
-
-  export {renderLineChart};
-
-
-
-//   import React, { PureComponent } from 'react';
-// import {
-//   LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
-// } from 'recharts';
 
 // const data = [
-//   {
-//     name: 'Page A', uv: 4000, pv: 2400, amt: 2400,
-//   },
-//   {
-//     name: 'Page B', uv: 3000, pv: 1398, amt: 2210,
-//   },
-//   {
-//     name: 'Page C', uv: 2000, pv: 9800, amt: 2290,
-//   },
-//   {
-//     name: 'Page D', uv: 2780, pv: 3908, amt: 2000,
-//   },
-//   {
-//     name: 'Page E', uv: 1890, pv: 4800, amt: 2181,
-//   },
-//   {
-//     name: 'Page F', uv: 2390, pv: 3800, amt: 2500,
-//   },
-//   {
-//     name: 'Page G', uv: 3490, pv: 4300, amt: 2100,
-//   },
+//   { name: 'Group A', value: 400 }, { name: 'Group B', value: 300 },
+//   { name: 'Group C', value: 300 }, { name: 'Group D', value: 200 },
 // ];
+// let data=[{name:'shari',value:2},{name:'Giuliano',value:3}];
 
-// export default class Example extends PureComponent {
-//   static jsfiddleUrl = 'https://jsfiddle.net/alidingling/xqjtetw0/';
 
-//   render() {
-//     return (
-//       <LineChart
-//         width={500}
-//         height={300}
-//         data={data}
-//         margin={{
-//           top: 5, right: 30, left: 20, bottom: 5,
-//         }}
-//       >
-//         <CartesianGrid strokeDasharray="3 3" />
-//         <XAxis dataKey="name" />
-//         <YAxis />
-//         <Tooltip />
-//         <Legend />
-//         <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-//         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-//       </LineChart>
-//     );
-//   }
-// }
+
+class ChartPie extends PureComponent {
+  
+
+  render() {
+    
+    
+    let data= this.props.data;
+    
+    let colours =['darkcyan','green'];
+
+  
+
+
+    if(data)
+    {
+
+      console.log(data);
+      return (
+        
+        <ResponsiveContainer height={200}>
+       <PieChart>
+        
+         <Pie label labelLine={false}  isAnimationActive={true} nameKey='name' dataKey='value' cx="50%" cy="50%" data={data} outerRadius={40} innerRadius={5}>
+           {
+              data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colours[index]}/>
+            ))
+            }
+         </Pie>
+         <Tooltip />
+         <Legend></Legend> 
+       </PieChart>
+       
+       </ResponsiveContainer> 
+      
+     );
+    }
+    else
+    {
+      return(<div></div>)
+    }
+    
+  }
+}
+
+export {ChartPie};
+
+
+
+
+
