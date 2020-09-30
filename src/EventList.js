f48764b0d013be364e8b62ab94ed3ee89c8fa00e
skouroufexis <skouroufexis@gmail.com>
2020-09-30 03:40:35 +0300

    Update EventList.js

--- src/EventList.js
+++ src/EventList.js
@@ -3,43 +3,43 @@ import Event from './Event';
 // import {mockEvents} from './mock-events';
 
 class EventList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {eventCounter: 0};
+  }
     
   render() {
     if (this.props.events.length>0)
       {
-
+                
         let totalEvents=this.props.events;
         let n=this.props.eventsNumber;
             if(n==''){n=32;}
             if(isNaN(n)){n=32;}
-        let c;
+        let c; //counter
+        
         let shownEvents=[];
 
         for (c=0;c<n;c++)
         {
           if(c<totalEvents.length)
           {
+            
             shownEvents.push(totalEvents[c]);
+            
           }
           
         }
-
-        console.log(shownEvents);
-
       return (
           <ul className="EventList">
-          {shownEvents.map(event =>
+          {shownEvents.map((event,index) =>
             <li key={event.id}>
-              <Event className='Event' event={event} />
+
+              <Event id={index}  className='Event' event={event}  />
             </li>
           )}
         </ul>
-
-        // <ul className="EventList">
-        //   <li key={this.props.events[0].id}>
-        //       <Event className='Event' event={this.props.events[0]} />
-        //     </li>
-        // </ul>
       );
     }
 
@@ -48,6 +48,7 @@ class EventList extends Component {
       return 'loading';
     }
   }
+
 }
 
 export default EventList;
